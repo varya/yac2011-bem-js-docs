ccb272ef56468170fa1854f6ea1e2568a60a895d
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-12 00:31:22 +0400

    New version of js component for cut

--- blocks/b-cut/b-cut.js
+++ blocks/b-cut/b-cut.js
@@ -7,7 +7,7 @@ BEM.DOM.decl('b-cut', {
 
     onSetMod : {
         'js' : function() {
-            BEM.blocks['b-link'].liveCtxBind(this.elem('opener'), 'click', this.open, this);
+            BEM.blocks['b-link'].on(this.elem('opener'), 'click', this.open, this);
             this.bindTo(this.elem('close'), 'leftclick', this.close);
         }
     },
@@ -15,14 +15,14 @@ BEM.DOM.decl('b-cut', {
         this.setMod(this.elem('content'), 'visibility', 'visible');
         this.setMod(this.elem('opener'), 'visibility', '');
     },
-    close: function() {
+    close : function() {
         this.setMod(this.elem('content'), 'visibility', '');
         this.setMod(this.elem('opener'), 'visibility', 'visible');
     }
 
 }, {
 
-    live : function() {
+    live: function() {
         this.liveInitOnBlockInsideInit('b-link');
     }
 
